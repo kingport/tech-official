9564905368a1642a6cefa81d90656aa64d876b30
wangdingkun <5446743+wangdingkun@user.noreply.gitee.com>
2022-07-05 21:47:56 +0800

    feat: add page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import { Mousewheel, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+
+import "swiper/css";
+import "swiper/css/pagination";
+import './index.css'
 
 function Home() {
   return (
-    <div className="home">home</div>
+    <div className="home">
+      <Swiper
+        direction={"vertical"}
+        slidesPerView={1}
+        spaceBetween={30}
+        mousewheel={true}
+        // pagination={{
+        //   clickable: false,
+        // }}
+        modules={[Mousewheel, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+    </div>
   )
 }
 
