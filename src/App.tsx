c764b5cc9a3b8e7e6f5b116881a648f09ff3baac
王定坤 <wangdingkun@huize.com>
2022-07-07 17:28:39 +0800

    feat: confirme home page

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/tech-official" element={<Home />} />
         <Route path="about" element={<About />} />
       </Routes>
     </div>
