1c956d82907957f8a88231f7d26550e67a55be2f
kingport <675468749@qq.com>
2022-07-07 09:24:20 +0000

    Deploying to gh-pages from @ kingport/tech-official@0fbf49f6e0d09c26fee32390f0e32df7a39c8198 🚀

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-const px2rem = require('postcss-px2rem')
-const postcss = px2rem({
-    remUnit: 190 // 基准值
-})
-module.exports = {
-  plugins: {
-    // postcss
-  }
-}
--- src/apis/key.ts
+++
@@ -1,2 +0,0 @@
-export const RQ_MODULE_PREFIX = 'RQ_module_name'
-export const RQ_SEARCH_RESULT = 'RQ_search_result'
