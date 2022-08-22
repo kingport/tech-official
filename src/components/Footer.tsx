901e3d05167a60c29c4e77088cf37ec4bf63684d
王定坤 <wangdingkun@huize.com>
2022-08-22 17:49:58 +0800

    fix: 合并公共分支代码

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,7 @@ import './footer.css';
 export default function () {
   return (
     <div className="footer">
-      Copyright © 2022 Hello-Tech. All Rights Reserved.华宝新能
-      版权所有粤ICP备13084034号
+      Copyright © 2022-2030 RIWUCT Online. All Rights Reserved
     </div>
   );
 }
